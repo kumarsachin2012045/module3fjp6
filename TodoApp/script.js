fe6b3c39cbbccb6b403e6698b98e2dbdf19afc00
sachin <sv19158161@gmail.com>
2022-10-24 18:22:04 +0530

    add hua child

--- TodoApp/script.js
+++ TodoApp/script.js
@@ -34,9 +34,11 @@ ulTag.appendChild(liTag);
 // let ulTag=document.querySelector("ul");
 
 // inputTag.addEventListener("keydown",function(e){
-// let key=e.key;//jo key press kroge wo store kiya
+    //function me jo bhi event hota h wo milta hai jo event h wo ek  object hai
+// let key=e.key;//jo press kroge wo key me jata hai to
+// e.key se nikal liya aur key varible me store kar liya
 // if(key=="Enter"){//jab key press ho tab ye kam ho
-// let value=inputTag.value;//isse jo input kiye inputbox me uski value value me store kar li
+// let value=inputTag.value;//isse jo input kiye inputbox me uski value value me store ki
 // inputTag.value="";//store karne ke bad use empty kar diya
 // //ab hmne jo value store kiya tha use page par(ul me) add karna hai
 
