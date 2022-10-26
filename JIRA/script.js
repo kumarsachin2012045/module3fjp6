7fc8999a79ca86a5208f289c00e245e9949dafb1
sachin <sv19158161@gmail.com>
2022-10-26 12:53:29 +0530

    ticket addition

--- JIRA/script.js
+++ JIRA/script.js
@@ -1,25 +1,45 @@
 let addBtn=document.querySelector(".add-btn");
-let modal=document.querySelector(".modal-cont");
+let modalCont=document.querySelector(".modal-cont");
+let taskAreaCont=document.querySelector(".textarea-cont");
+let mainCont=document.querySelector(".main-cont")
 let addModal=true;//ye flag h
 addBtn.addEventListener("click",function(){
 if(addModal)
 {
     //show modal-cont ya writing box
-    modal.style.display="flex";
+    modalCont.style.display="flex";
 }
 else
 {
     //hide modal-cont ya writing box
-    modal.style.display="none";
+    modalCont.style.display="none";
 }
 addModal=!addModal;
 // har bar toggle karna padega true ho to false and false ho to true
-
-
 })
 
-
-
+modalCont.addEventListener("keydown",function(e){
+let key=e.key;
+if(key=="Enter"){
+  createTicket(taskAreaCont.value)
+  taskAreaCont.value="";
+  modalCont.style.display="none"
+  addModal=!addModal;
+}
+})
+function createTicket(value){
+//    <div class="ticket-cont">
+//     <div class="ticket-color"></div>
+//     <div class="ticket-id">#2z059</div>
+//     <div class="task-area">Task area</div>
+//     </div> 
+    let ticketCont=document.createElement("div");
+    ticketCont.setAttribute("class","ticket-cont");
+    ticketCont.innerHTML=`<div class="ticket-color"></div>
+                          <div class="ticket-id">#2z059</div>
+                          <div class="task-area">${value}</div>`
+    mainCont.appendChild(ticketCont);
+}
 
 
 
@@ -56,3 +76,4 @@ addModal=!addModal;
 //iska position set karna padega to position absolute se karenge mdn par jake
 //demo dekh lena https://developer.mozilla.org/en-US/docs/Web/CSS/position
 //baki ticket hta do bas ek rkho phir styling karna h pop up wala click krne pr
+//task h--hme ticket kab chahiye jab writing box me likh ke click kre tab chahiye
