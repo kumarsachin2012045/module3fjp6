98116539d67d9595989dd2e928ca4c3f15a04082
sachin <sv19158161@gmail.com>
2022-10-29 19:25:10 +0530

    ticket append kiya iske parent me

--- Jira Ticket/script.js
+++ Jira Ticket/script.js
@@ -1,15 +1,46 @@
 let addBtn=document.querySelector(".add-btn");
-let modal=document.querySelector(".modal-cont");
+let modalCont=document.querySelector(".modal-cont");
+let taskareaCont=document.querySelector(".textarea-cont");
+let mainCont=document.querySelector(".main-cont")
 let addModal=true;
 addBtn.addEventListener("click",function(){
 if(addModal){
    //show agr true h
-   modal.style.display="flex";
+   modalCont.style.display="flex";
 }
 else{
     //hide
-    modal.style.display="none";
+    modalCont.style.display="none";
 }
 addModal=!addModal;
 //above code se seekha toggale kaise karna hai
 })
+modalCont.addEventListener("keydown",function(e){
+    let key=e.key;
+    // console.log(key);
+   if(key=="Enter")
+   {
+    createTicket(taskareaCont.value);
+    modalCont.style.display="none";
+    taskareaCont.value="";
+    addModal=!addModal;   
+   }
+})
+function createTicket(task){
+    // <div class="ticket-cont">
+            // <div class="ticket-color green">black</div>
+            // <div class="ticket-id">#423c</div>
+            // <div class="task-area">some task</div>
+    //         </div>
+    //ye kam java script se karna hai
+    //create class
+    let ticketCont=document.createElement("div");
+    ticketCont.setAttribute("class","ticket-cont");
+    ticketCont.innerHTML=`<div class="ticket-color green">black</div>
+                           <div class="ticket-id">#423c</div>
+                            <div class="task-area">${task}</div>`
+//ticket ban gya javascript se ab ise append karna hai iske parent me 
+//to pahle use sellect kro sellect wala kam upar kiye hai
+mainCont.appendChild(ticketCont);
+
+}
