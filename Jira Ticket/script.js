85cb4c5db4ac3816c7c084ac871a5a5bf537f541
sachin <sv19158161@gmail.com>
2022-10-29 15:15:13 +0530

    ticket template ok

--- Jira Ticket/script.js
+++ Jira Ticket/script.js
@@ -2,11 +2,6 @@ let addBtn=document.querySelector(".add-btn");
 let modal=document.querySelector(".modal-cont");
 let addModal=true;
 addBtn.addEventListener("click",function(){
-// console.log("button is clicked");
-//jaise hi click ho modal ki styling chasnge karna h 
-//to phle modal ko select karna padega andar bahr khi bhi kr lo
-//modal.style.display="flex";//agya ab dubara click pr jana chahiye
-//ab hme toggle krna h to iske liye flag(addModal) lete h 
 if(addModal){
    //show agr true h
    modal.style.display="flex";
